cb24f69c735ab7edbce688ac7c811421ff6b8deb
shahz <shahzodergashev2555@gmail.com>
2024-07-25 13:34:07 +0300

    товары

--- sweater/routes.py
+++ sweater/routes.py
@@ -12,8 +12,7 @@ def home():
         products = Product.query.filter_by(category_id=category_id).order_by(Product.date.desc()).all()
     else:
         products = Product.query.order_by(Product.date.desc()).limit(9).all()
-    # categories = Category.query.join(Product).group_by(Category.id).all()
-    categories = Category.query.all()
+    categories = Category.query.join(Product).group_by(Category.id).all()
     favorite_product_ids = [fav.product_id for fav in current_user.favorites] if current_user.is_authenticated else []
     num_product = len(products)
 
